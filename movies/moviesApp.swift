b6a51199c364c9201732c701891ec5b1915d2002
Matis Bogaert <Matis.Bogaert@iMacD.local>
2025-12-02 09:17:59 +0100

    first commit

---
+++ movies/moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesApp.swift
+//  movies
+//
+//  Created by Matis Bogaert on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
